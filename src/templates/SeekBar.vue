378a6fb34f52560b541391901c856678020509f9
Mijinko_SD <79152329+MijinkoSD@users.noreply.github.com>
2023-09-10 02:25:59 +0900

    Add: シークバー

---
+++ src/templates/SeekBar.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+const props = defineProps<{
+  length: number
+  current: number
+}>()
+</script>
+
+<template>
+  <div class="seek-bar">
+    <div class="bar">
+      <div class="point-pos" :style="{ left: (current * 100) / length + '%' }">
+        <div class="point"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+$height: 5px;
+
+.seek-bar {
+  width: 100px;
+  height: $height;
+}
+
+.bar {
+  // border: 2px solid gray;
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  border-radius: calc($height / 2);
+  background-color: #ddd;
+}
+
+.point-pos {
+  position: relative;
+  top: (15px - $height) * -0.5;
+  width: 0;
+}
+
+.point {
+  position: relative;
+  left: calc(15px / -2);
+  margin-left: -50%;
+  width: 15px;
+  height: 15px;
+  border-radius: 50%;
+  background-color: gray;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-
+import SeekBar from '@/templates/SeekBar.vue'
 </script>
 
 <template>
   <p>Home View.</p>
+  <SeekBar :length="180" :current="24" />
 </template>
