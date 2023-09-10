fcc1cceae65d157f4afd149241c858374ddbd7fc
Mijinko_SD <79152329+MijinkoSD@users.noreply.github.com>
2023-09-10 21:01:12 +0900

    Update: プロキシ経由でバックエンドに接続できるように

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +12,17 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    // FIXME: 【最優先】修正して
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
