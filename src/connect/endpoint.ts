e8c61051175d6a4e3b342519aacd5bb16ea22a81
Mijinko_SD <79152329+MijinkoSD@users.noreply.github.com>
2023-09-10 18:40:16 +0900

    Fix: `getM3u8Json`の戻り値の型の設定間違い

--- src/connect/endpoint.ts
+++ src/connect/endpoint.ts
@@ -3,7 +3,9 @@ import rest from "./axiosInit";
 import type { MovieInfo } from "@/types/m3u8";
 
 const endpoint = {
-  "getM3u8Json": async (liveId: string): Promise<AxiosResponse<MovieInfo>> => {
+  "getM3u8Json": async (
+    liveId: string,
+  ): Promise<AxiosResponse<MovieInfo[]>> => {
     return rest.get(`/archive/${liveId}/m3u8/json`);
   },
 };
