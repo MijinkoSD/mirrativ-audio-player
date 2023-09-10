540c2fb742012e423956009dc41865d1cdac8ee6
Mijinko_SD <79152329+MijinkoSD@users.noreply.github.com>
2023-09-10 16:49:11 +0900

    Add: シークバー

--- src/templates/SeekBar.vue
+++ src/templates/SeekBar.vue
@@ -1,16 +1,74 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
 const props = defineProps<{
   length: number
   current: number
 }>()
+const emits = defineEmits<{
+  /** バーが随時操作されている際に発生するイベント */
+  changing: [current: number, length: number]
+  /** バーの操作が完了した際に発生するイベント */
+  changed: [current: number, length: number]
+  /** currentの値が変更された際に発生するイベント */
+  'update:current': [newValue: number]
+}>()
+
+/** シークバーの進捗割合 */
+let progress = props.current / props.length
+/** .barの参照 */
+const barDom = ref<HTMLDivElement>()
+/** バーがクリックされていればtrue */
+let isClicking: boolean = false
+
+/** バーが操作された際のイベント */
+const onMouseAction = (e: MouseEvent) => {
+  // if(e.target !== e.currentTarget) return
+  if (barDom.value === undefined) return
+  const width = barDom.value.clientWidth
+  // const posX = e.offsetX
+  const rect = barDom.value.getBoundingClientRect()
+  const posX = e.clientX - rect.left
+  progress = posX / width
+
+  if (progress < 0) progress = 0
+  else if (progress > 1) progress = 1
+}
+
+const onMouseDown = () => {
+  isClicking = true
+}
+const onMouseUp = () => {
+  isClicking = false
+}
+const onMouseMove = (e: MouseEvent) => {
+  if (!isClicking) return
+  onMouseAction(e)
+  const newCurrent = props.length * progress
+  if (isClicking) {
+    // クリック中の際は変更中のイベントを発生
+    emits('changing', newCurrent, props.length)
+  } else {
+    // クリック済みの際は変更済みのイベントを発生
+    emits('changed', newCurrent, props.length)
+  }
+  emits('update:current', newCurrent)
+}
+
+// マウスクリックを終了した際のイベントはドキュメント全体で取得できるようにする
+document.addEventListener('mouseup', onMouseUp)
+document.addEventListener('mouseleave', onMouseUp)
+// document.addEventListener("pointerdown", onMouseDown)
+document.addEventListener('mousemove', onMouseMove)
 </script>
 
 <template>
-  <div class="seek-bar">
-    <div class="bar">
-      <div class="point-pos" :style="{ left: (current * 100) / length + '%' }">
-        <div class="point"></div>
-      </div>
+  <div class="seek-bar" @mousedown="onMouseDown">
+    <div class="bar" ref="barDom">
+      <div class="progress" :style="{ width: progress * 100 + '%' }" />
+    </div>
+    <div class="point-pos" :style="{ left: progress * 100 + '%' }">
+      <div class="point"></div>
     </div>
   </div>
 </template>
@@ -19,22 +77,31 @@ const props = defineProps<{
 $height: 5px;
 
 .seek-bar {
-  width: 100px;
-  height: $height;
+  padding-top: 10px;
+  display: block;
+  width: 100%;
+  height: calc(10px + $height);
 }
 
 .bar {
-  // border: 2px solid gray;
-  box-sizing: border-box;
   width: 100%;
-  height: 100%;
+  height: $height;
   border-radius: calc($height / 2);
   background-color: #ddd;
+  overflow: hidden;
+
+  .progress {
+    position: relative;
+    left: 0;
+    background-color: hsl(140 71% 60% / 1);
+    width: 50%;
+    height: 100%;
+  }
 }
 
 .point-pos {
   position: relative;
-  top: (15px - $height) * -0.5;
+  top: (15px - $height) * -1;
   width: 0;
 }
 
@@ -46,5 +113,7 @@ $height: 5px;
   height: 15px;
   border-radius: 50%;
   background-color: gray;
+  // ↓これが無いとイベントがバグる
+  pointer-events: none;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,25 @@
 <script setup lang="ts">
 import SeekBar from '@/templates/SeekBar.vue'
+import { computed, ref } from 'vue'
+
+const _current = ref<number>(24)
+const current = computed({
+  get: () => {
+    return _current.value
+  },
+  set: (value) => {
+    _current.value = Math.round(value)
+  }
+})
 </script>
 
 <template>
   <p>Home View.</p>
-  <SeekBar :length="180" :current="24" />
+  <SeekBar class="seek-bar" v-for="i in 1" :key="i" :length="180" v-model:current="current" />
 </template>
+
+<style scoped lang="scss">
+.seek-bar {
+  max-width: 200px;
+}
+</style>
